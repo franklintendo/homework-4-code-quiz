d510131b47340010cc91335732a565d6179b5c06
Frank Moran <info@francisfrankmoran.com>
2020-01-07 16:42:06 -0500

    setting up index

---
+++ assets/questions.js
@@ -0,0 +1,17 @@
+var questions = [
+    {
+      title: "Which method would you use to find an ID element in HTML",
+      choices: ["getElementsById()", "getElementByID()", "getElementbyId()", "getElementsById()"],
+      answer: "getElementbyId()"
+    },
+    {
+      title: "To see if two variables are equal in an if / else statement you would use ____.",
+      choices: ["=", "==", "'equals'", "!="],
+      answer: "=="
+    },
+    {
+        title: "Math.random() returns ____.",
+        choices: ["a number between 1 and 9", "a number between 0 and 9", "a number between 0 and 1", "a number between 0 and 99"],
+        answer: "a number between 0 and 1"
+    }
+  ];
